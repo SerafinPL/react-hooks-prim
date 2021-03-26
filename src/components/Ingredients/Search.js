250d7183bb7ebf8feb2fa5d409eb29a2938907d5
serafinpl <ser.burberibar@gmail.com>
2021-03-26 22:06:38 +0100

    slow down requests by timer

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
@@ -9,26 +9,40 @@ const Search = React.memo(props => {
 
   const [inputState, setInputState] = useState('');
 
-  useEffect(() => {
-    const query = inputState.length === 0 ? '' : `?orderBy="title"&equalTo="${inputState}"`;
-    fetch('https://hooks-e900b-default-rtdb.firebaseio.com/ingredients.json' + query)
-      .then(response => response.json())
-        .then(responseData => {
-          const loadedIngredients = [];
-          for (const key in responseData){
-            loadedIngredients.push({id: key, ...responseData[key]})
-          }
-          onLoadIngr(loadedIngredients);
-        }); 
+  const inputRef = useRef();
 
-  }, [inputState, onLoadIngr]);
+  useEffect(() => {
+
+    const timer = setTimeout(() => {
+      if(inputState === inputRef.current.value){
+          const query = inputState.length === 0 ? '' : `?orderBy="title"&equalTo="${inputState}"`;
+          fetch('https://hooks-e900b-default-rtdb.firebaseio.com/ingredients.json' + query)
+            .then(response => response.json())
+              .then(responseData => {
+                const loadedIngredients = [];
+                for (const key in responseData){
+                  loadedIngredients.push({id: key, ...responseData[key]})
+                }
+                onLoadIngr(loadedIngredients);
+              });
+      }
+       
+    }, 500);
+    return () => {clearTimeout(timer);}
+
+  }, [inputState, onLoadIngr, inputRef]);
 
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" value={inputState} onChange={event => setInputState(event.target.value)}/>
+          <input 
+            ref={inputRef}
+            type="text" 
+            value={inputState} 
+            onChange={event => setInputState(event.target.value)}
+          />
         </div>
       </Card>
     </section>
